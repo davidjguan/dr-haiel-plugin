de18edd9d253e97a1f4def994c795a43eeffa00e
davidjguan <dguan@umd.edu>
2024-08-12 11:08:41 -0400

    updated react app which now automatically includes the build-non-split stuff and has new run build-for-plugin command

--- hello.php
+++ hello.php
@@ -24,8 +24,8 @@ add_shortcode('haiel', 'helloworld_shortcode');
 
 function helloworld_load_assets() {
 	
-	$react_app_js  = plugin_dir_url( __FILE__ ) . 'updateddrhaiel/build/static/js/all_in_one_file.js';
-    $react_app_css = plugin_dir_url( __FILE__ ) . 'updateddrhaiel/build/static/css/somecss.css';
+	$react_app_js  = plugin_dir_url( __FILE__ ) . 'ls_uhcc_bot_fe/build/static/js/all_in_one_file.js';
+    $react_app_css = plugin_dir_url( __FILE__ ) . 'ls_uhcc_bot_fe/build/static/css/somecss.css';
     // time stops stylesheet/js caching while in development, might want to remove later  
     $version = time();	
     wp_enqueue_script( 'haiel', $react_app_js, array(), $version, true );         
